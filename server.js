ce81305a0993c34f18f16f8d62b5effba4478e4e
John <JLSenar@gail.com>
2018-08-24 16:29:30 -0700

    yarn add path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 const yelp = require('yelp-fusion');
@@ -34,6 +35,8 @@ app.get('/api/search/:location', (req, res) => {
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
+
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
