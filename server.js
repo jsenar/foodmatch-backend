3dfc8719fdbe145428ba8c8fadc45eacfa66522c
John <JLSenar@gail.com>
2018-08-24 15:47:24 -0700

    initial heroku deploy

--- server.js
+++ server.js
@@ -2,17 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const yelp = require('yelp-fusion');
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost/foodmatch'
+var mongoose = require('mongoose');
+const url = 'mongodb://localhost/foodmatch';
+var MongoClient = require('mongodb').MongoClient
 
-//MongoDB test
-MongoClient.connect(url, function(err, db){
-  db.close();
-});
-
-
-var keys = require('./config.js');
-apiKey = keys.yelp_key;
+const keys = require('./config.js');
+const apiKey = keys.yelp_key;
 
 var searchRequest = {
   term:'restaurants',
@@ -22,11 +17,11 @@ var searchRequest = {
   limit: 5
 };
 
-const client = yelp.client(apiKey);
+const yelpClient = yelp.client(apiKey);
 
 app.get('/api/search/:location', (req, res) => {
   searchRequest.location = req.params.location;
-  client.search(searchRequest).then(response => {
+  yelpClient.search(searchRequest).then(response => {
     const firstResult = response.jsonBody.businesses;
     const prettyJson = JSON.stringify(firstResult, null, 4);
     res.send({businesses: prettyJson})
